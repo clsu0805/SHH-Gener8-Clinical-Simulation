6899f55fd63305cd702dc90af3a0c46e69e3e321
clsu0805 <clsu0805@gmail.com>
2026-09-25 11:44:22 +0800

    feat: add AECOPD media manifest

---
+++ scenarios/aecopd/data/media-manifest.js
@@ -0,0 +1,12 @@
+window.MEDIA_MANIFEST = Object.freeze({
+act1InitialPatient:'assets/video/media1.mp4',
+act1VitalsPatient:'assets/video/media2.mp4',
+act1EvidencePatient:'assets/video/media3.mp4',
+act1PostTreatmentPatient:'assets/video/media5.mp4',
+act2Monitor:'assets/video/media6.mp4',
+act2IntroPatient:'assets/video/media7.mp4',
+act2Patient:'assets/video/media8.mp4',
+nivPatient:'assets/video/media9.mp4',
+nivMonitor:'assets/video/media10.mp4',
+lungSound:'assets/audio/media4.m4a'
+});
